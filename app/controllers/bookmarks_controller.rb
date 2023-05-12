3683c34c26794a610432ff3a71f1fd3189a6e060
angelacuahutleboardspancom <angela.cuahutle@gmail.com>
2023-05-12 14:43:15 -0600

    Add functionality to sticky header table, first row, filter tagsshow less than 10 for design purposes and add description column

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -73,15 +73,9 @@ class BookmarksController < ApplicationController
 
   private
 
-
-  # Use callbacks to share common setup or constraints between actions.
-
   def set_bookmark
     @bookmark = Bookmark.find(params[:id])
   end
-
-
-  # Only allow a list of trusted parameters through.
 
   def bookmark_params
     params.require(:bookmark).permit(:href, :description, :extended, :meta, :hash_value, :shared, :toread, :tags[])
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,20 +8,22 @@ class PagesController < ApplicationController
     website_names_with_tags = bookmarks.map do |bookmark|
       url = bookmark.href
       website_name = url.sub(%r{^https?://(www\.)?}, '').split('/').first
-      { website_name: website_name, tags: bookmark.tags }
+      { website_name: website_name, tags: bookmark.tags, description: bookmark.description }
     end
 
     @sorted_website_names_with_tags = website_names_with_tags.group_by { |entry| entry[:website_name] }.transform_values do |entries|
-      entries.flat_map { |entry| entry[:tags] }.tally
+      tags_tally = entries.flat_map { |entry| entry[:tags] }.tally
+      descriptions = entries.map { |entry| entry[:description] }.uniq
+      { tags: tags_tally, descriptions: descriptions }
     end
 
     @sorted_website_names_with_tags = case sort_column
-    when "website_name"
+    when 'website_name'
       @sorted_website_names_with_tags.sort_by { |website_name, _| website_name }
-    when "count"
-      @sorted_website_names_with_tags.sort_by { |_, tags_tally| tags_tally.values.sum }
-    when "tags"
-      @sorted_website_names_with_tags.sort_by { |_, tags_tally| tags_tally.keys }
+    when 'count'
+      @sorted_website_names_with_tags.sort_by { |_, data| data[:tags].values.sum }
+    when 'tags'
+      @sorted_website_names_with_tags.sort_by { |_, data| data[:tags].keys }
     end
 
     @sorted_website_names_with_tags = @sorted_website_names_with_tags.reverse! if sort_direction == "desc"
